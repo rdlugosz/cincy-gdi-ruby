4329ca81682210c4e79a52f88f7ed19f44a7225c
Ryan Dlugosz <ryan@dlugosz.net>
2016-04-02 01:30:21 -0400

    Update greet script

--- greeter.rb
+++ greeter.rb
@@ -10,12 +10,17 @@ class Greeter
     end
 
     student_names.each do |student_name|
-      puts "Hi #{student_name}, it is #{random_adverb} to meet you!"
-      sleep 1
+      puts "Hi #{student_name}, it is #{random_adverb.upcase} to meet you!"
+      sleep 1.4
     end
 
-    puts "\nAlright everyone. Let's get started."
+    puts
+    puts "Alright everyone. Let's get started."
+
+    sleep 2
+
     puts "Soon all #{student_names.size} of you will know a little Ruby!"
+    puts
   end
 
   def random_adverb
