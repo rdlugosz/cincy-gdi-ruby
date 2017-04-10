217110f1b2d16db47bb63e39e8ee21e82da93781
Ryan Dlugosz <ryan@dlugosz.net>
2017-04-10 14:57:36 -0400

    Add volunteers

--- classroom_manager_rails/app/models/classroom.rb
+++ classroom_manager_rails/app/models/classroom.rb
@@ -1,4 +1,12 @@
 class Classroom < ActiveRecord::Base
   has_many :enrollments
   has_many :students, through: :enrollments
+
+  def has_students?
+    students.count > 0
+  end
+
+  def volunteer
+    students.sample
+  end
 end
